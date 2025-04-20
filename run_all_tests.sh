e3ef6d46f0dc5bd3e0d813f4084baca13441b851
Mayaaa311 <mayaaa@umich.edu>
2025-04-20 10:43:50 -0400

    30min cutoff finished bnb

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -4,6 +4,6 @@ DATA_DIR="data"
 for file in "$DATA_DIR"/*.in; do
     if [[ -f "$file" ]]; then
 
-        python3 main.py -inst "$file" -alg BnB -time 600
+        python3 main.py -inst "$file" -alg BnB -time 1800
     fi
 done
